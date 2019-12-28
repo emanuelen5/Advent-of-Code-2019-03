a6113fa45a1986b3a3b24c86ca632cda4ace838c
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-12-28 12:54:13 +0100

    Refactor

--- main.py
+++ main.py
@@ -37,23 +37,21 @@ def get_move_type(coord_from, coord_to):
     else:
         return MOVE_DIAGONAL
 
+def get_intersection_solution(h_coord_from, h_coord_to, v_coord_from, v_coord_to):
+    h_move_span = sorted([h_coord_from[0], h_coord_to[0]])
+    v_move_span = sorted([v_coord_from[1], v_coord_to[1]])
+    if h_move_span[0] <= v_coord_from[0] <= h_move_span[1] and v_move_span[0] <= h_coord_from[1] <= v_move_span[1]:
+        return [v_coord_from[0], h_coord_from[1]]
+    else:
+        return None
+
 def get_intersection(coord1_from, coord1_to, coord2_from, coord2_to):
     move_type1 = get_move_type(coord1_from, coord1_to)
     move_type2 = get_move_type(coord2_from, coord2_to)
     if move_type1 == MOVE_HORIZONTAL and move_type2 == MOVE_VERTICAL:
-        h_move_span = sorted([coord1_from[0], coord1_to[0]])
-        v_move_span = sorted([coord2_from[1], coord2_to[1]])
-        if h_move_span[0] <= coord2_from[0] <= h_move_span[1] and v_move_span[0] <= coord1_from[1] <= v_move_span[1]:
-            return [coord2_from[0], coord1_from[1]]
-        else:
-            return None
+        return get_intersection_solution(coord1_from, coord1_to, coord2_from, coord2_to)
     elif move_type1 == MOVE_VERTICAL and move_type2 == MOVE_HORIZONTAL:
-        h_move_span = sorted([coord2_from[0], coord2_to[0]])
-        v_move_span = sorted([coord1_from[1], coord1_to[1]])
-        if h_move_span[0] <= coord1_from[0] <= h_move_span[1] and v_move_span[0] <= coord2_from[1] <= v_move_span[1]:
-            return [coord1_from[0], coord2_from[1]]
-        else:
-            return None
+        return get_intersection_solution(coord2_from, coord2_to, coord1_from, coord1_to)
     else:
         return None
 
